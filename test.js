8ef0078454575f42b9dfd2dec2a5ec533a4fe495
Ali Zia <141221368+alizia-dev@users.noreply.github.com>
2025-10-29 16:49:45 +0500

    Create test.js with Node.js usage instructions
    Add initial test.js file with Node.js usage comments

---
+++ test.js
@@ -0,0 +1,5 @@
+// This is how we can use javascript files to run using node.js,
+// previously we used web browsers to run javascript code.
+// Now we can run javascript code directly on our computer using node.js
+// The whole web browser environment is not needed to run javascript code.
+console.log("This is a test file.");
